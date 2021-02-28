2ebff1963a96e6c22c16fcf2523c0888b5795e50
Jean-Philippe Belval <2037155@etu.cegepsth.qc.ca>
2021-02-27 20:46:48 -0500

    Add button

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 var timer = setInterval(changeImage, 5000)
 var num = 2;
 var nbimage = 8;
+var start = false;
 
 function changeImage(){
-    if(num < nbimage){
-        document.getElementById("legif").src="gifs/kermit"+ num +".gif";
-        console.log("yep");
-        num++;
+    if(start){
+        if(num < nbimage){
+            document.getElementById("legif").src="gifs/kermit"+ num +".gif";
+            console.log("yep");
+            num++;
+        }
+        else if(num = nbimage){
+            num = 1;
+        }
     }
-    else if(num = nbimage){
-        num = 1;
+    else{
+        console.log("Nah")
     }
+}
+
+function play(){
+    var audio = document.getElementById("audio");
+    var image = document.getElementById("legif");
+    var button = document.getElementById("buttonplay")
+    image.style.display = "block";
+    button.style.display = "none";
+    start = true;
+    audio.play();
+    console.log("Pressed");
 }
